41339c82a160013a53a3de92cbcfe2029b887899
xmas7 <xhunter0228@gmail.com>
2022-01-24 05:43:28 +0800

    Complete All Project

--- frontend/src/pages/Products.js
+++ frontend/src/pages/Products.js
@@ -1,101 +1,129 @@
 
 import { useState } from 'react';
 import { useEffect } from 'react';
-// material
-import { Container, Stack } from '@mui/material';
-// components
+import { Button, Stack, Grid } from '@mui/material';
 import Page from '../components/Page';
-import {
-  ProductSort,
-  ProductList,
-} from '../components/_dashboard/products';
-
-import TransitionsModal from '../components/TransitionsModal';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
+import { useWallet } from "@solana/wallet-adapter-react";
 import {
-  ModalContext
-} from '../contexts/ProductModalContext';
-
-import { getFormattedPrice } from '../contexts/utils'
-
-import {
-  getFullContents, getContent
+  initProject, 
+  stakeToLottery, 
+  withdrawFromLottery, 
+  stakeToFixed, 
+  withdrawFromFixed, 
+  getLotteryState, 
+  getFixedState, 
+  getGlobalState
 } from '../contexts/helpers';
-
-// ----------------------------------------------------------------------
+import { getDateStr, getReward } from '../contexts/utils';
 
 export default function EcommerceShop() {
-  
-  const [openFilter, setOpenFilter] = useState(false);
-  const [contents, setContents] = useState([]);
-  const [modalOpened, setModalOpened] = useState(false);
-  const [isDetailModal, setIsDetailModal] = useState(true);
-  const toggleModal= ()=>{
-    setModalOpened(!modalOpened);
-  }
-  const [nftModalInfo, setNftModalInfo] = useState({});
+  const [lotteryNftMint, setLotteryNFTMint] = useState("");
+  const [fixedNftMint, setFixedNFTMint] = useState("");
+  const [lotteryState, setLotteryState] = useState({ itemCount: 0, items: [] });
+  const [fixedState, setFixedState] = useState({ itemCount: 0, items: [] });
+  const [globalState, setGlobalState] = useState({ lotteryNftCount: 0, fixedNftCount: 0});
+  const wallet = useWallet();
 
   useEffect(() => {
-    getFullContents().then((arrRecords) => {
-      setContents(arrRecords);
-    });
-  }, []);
+    updateLotteryPoolState(wallet.publicKey);
+    updateFixedPoolState(wallet.publicKey);
+  }, [wallet])
 
-  const updateData = (
-    hero_id, listed_price, content_uri, owner 
-  ) => {
-    console.log("updateData ", hero_id, listed_price, content_uri, owner);
-    let tempContents = contents;
-    tempContents[hero_id].listed_price = listed_price;
-    tempContents[hero_id].owner = owner;
-    if (contents[hero_id].content_uri !== content_uri) {
-      console.log("updateData content_uri not same");
-      getContent(tempContents[hero_id]).then((tempRecord) => {
-        tempContents[hero_id] = tempRecord;
-        console.log("updateData content_uri fetched", tempRecord);
-        setContents(tempContents);
-      });
-    } else {    
-      tempContents[hero_id].content_uri = content_uri;
-      setContents(tempContents);
-    } 
-    /*getFullContents().then((arrRecords) => {
-      console.log("updateDAta =", arrRecords);
-      console.log(arrRecords[2].listed_price.toNumber());
-      console.log(getFormattedPrice(arrRecords[2].listed_price));
-      setContents(arrRecords);
-    });*/
-    
+  const updateLotteryPoolState = (addr) => {
+    console.log("updateLotteryPoolState");
+    getLotteryState(addr).then(result => {
+      if(result !== null) {
+        setLotteryState({
+          itemCount: result.itemCount.toNumber(),
+          items: result.items.slice(0, result.itemCount.toNumber())
+        })
+      }
+    })
+    getGlobalState().then(result => {
+      setGlobalState({
+        lotteryNftCount: result.lotteryNftCount.toNumber(),
+        fixedNftCount: result.fixedNftCount.toNumber()
+      })
+    })
   }
-
+  const updateFixedPoolState = (addr) => {
+    getFixedState(addr).then(result => {
+      if(result !== null) {
+        setFixedState({
+          itemCount: result.itemCount.toNumber(),
+          items: result.items.slice(0, result.itemCount.toNumber())
+        })
+      }
+    })
+    getGlobalState().then(result => {
+      setGlobalState({
+        lotteryNftCount: result.lotteryNftCount.toNumber(),
+        fixedNftCount: result.fixedNftCount.toNumber()
+      })
+    })
+  }
+  
+  const onInitClick = () => {
+    initProject(wallet).then(() => {
+      updateLotteryPoolState(wallet.publicKey);
+      updateFixedPoolState(wallet.publicKey);
+    });
+  }
+  const onStakeToLottery = () => {
+    stakeToLottery(wallet, lotteryNftMint).then(() => {
+      updateLotteryPoolState(wallet.publicKey)
+    });
+  }
+  const onWithdrawFromLottery = () => {
+    withdrawFromLottery(wallet, lotteryNftMint).then(() => {
+      updateLotteryPoolState(wallet.publicKey)
+    });
+  }
+  const onStakeToFixed = () => {
+    stakeToFixed(wallet, fixedNftMint).then(() => {
+      updateFixedPoolState(wallet.publicKey)
+    });
+  }
+  const onWithdrawFromFixed = () => {
+    withdrawFromFixed(wallet, fixedNftMint).then(() => {
+      updateFixedPoolState(wallet.publicKey)
+    });
+  }
   return (
-    <ModalContext.Provider 
-      value = {{
-        toggleModal,
-        setNftModalInfo,
-        modalOpened,
-        nftModalInfo,
-        isDetailModal,
-        setIsDetailModal,
-        updateData,
-        contents
-      }}>
       <Page title="Dashboard: Products | Minimal-UI" ml={5}>
-          <Stack
-            direction="row"
-            flexWrap="wrap-reverse"
-            alignItems="center"
-            justifyContent="flex-end"
-          >
-            <Stack direction="row" spacing={1} flexShrink={0} sx={{ my: 1 }}>
-              <ProductSort />
-            </Stack>
-          </Stack>
-
-          <ProductList/>
-          <TransitionsModal/>
+          <button onClick={() => onInitClick()}>Init Project</button>
+          <Grid container spacing={2}>
+            <Grid item xs={5}>
+              <Stack direction="column" spacing={2}>
+                <h3>Lottery Pool : Total {globalState.lotteryNftCount}</h3>
+                <input type="text" id="nft_mint" value={lotteryNftMint} onChange={(e) => setLotteryNFTMint(e.target.value)}></input>
+                <button onClick={() => onStakeToLottery()}>Stake To Lottery Pool</button>
+                <button onClick={() => onWithdrawFromLottery()}>Withdraw From Lottery Pool</button>
+                <h5>My Staked NFTs in Lottery: {lotteryState.itemCount}</h5>
+                {
+                  lotteryState.items.map((item, id) => (
+                      <h6 key={id}>{getDateStr(item.stakeTime) + " >>> " + item.nftAddr.toBase58()}</h6>
+                  ))
+                }
+              </Stack>
+            </Grid>
+            <Grid item xs={5}>  
+              <Stack direction="column" spacing={2}>
+                  <h3>Fixed Pool : Total {globalState.fixedNftCount}</h3>
+                  <input type="text" id="nft_mint1" value={fixedNftMint}  onChange={(e) => setFixedNFTMint(e.target.value)}></input>
+                  <button onClick={() => onStakeToFixed()}>Stake To Fixed Pool</button>
+                  <button onClick={() => onWithdrawFromFixed()}>Withdraw From Fixed Pool</button>
+                  <h5>My Staked NFTs in Fixed Pool: {fixedState.itemCount}</h5>
+                  {
+                    fixedState.items.map((item, id) => (
+                        <h6 key={id}>{getDateStr(item.stakeTime) + "<<<  " + getReward(item.stakeTime) + "SOL  >>> " + item.nftAddr.toBase58()}</h6>
+                    ))
+                  }
+              </Stack>
+            </Grid>
+          </Grid>
           <ToastContainer
             position="top-right"
             autoClose={5000}
@@ -108,6 +136,5 @@ export default function EcommerceShop() {
             pauseOnHover
           />
       </Page>
-    </ModalContext.Provider>
   );
 }
